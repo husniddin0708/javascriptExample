4b43d4554e0c15b037067a0280b3d95e8a63af3c
husniddin0708 <110809587+husniddin0708@users.noreply.github.com>
2022-08-12 11:57:50 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,78 @@
-//vaqtga qarab salom berish
-let hour =prompt("Vaqtni kiritng")
-if (hour<12){
-    console.log("Good morning");
-} else if(hour<18){
-    console.log("Good afternoon");
-}else{
-    console.log("Good night")
-}
-//yoshingizga qarab saytga kiritish
-let yosh = prompt("Yoshingizni kiriting");
-let hisobla = yosh<18 ? "Sizga kirish mumkin emas":"Welcome to my site";
-confirm(hisobla);
-// string metodlari
-let a = "text       ";
-let b = "Again text";
-console.log(a.concat(b));
-console.log(a.length);
-console.log(b.length);
-console.log(a.slice(3));
-console.log(a.slice(2));
-console.log(a.slice(1));
-console.log(a);
-console.log(a.repeat(2));
-console.log(a.repeat(2).length);
-console.log(b.slice(5).concat(a));
-console.log(a.replace("text", "changed text"));
-console.log(a.padStart(20, "stupid"))
-console.log(a.concat("is").padEnd(20, "stupid"));
-console.log(a.substring(0, 2).concat(b.slice(8)));
-console.log(a.includes("t"));
-console.log(a.endsWith("ghdcfoubgvoykgu8'uyhgfhgdtfkyig,h   "));
-console.log(a.concat(b).trim());
-console.log(a.substring(0, 2).repeat(10));
-let number =1234567;
-console.log(number.toString())
-let textNumber ="1234567";
-console.log(Number(textNumber))
-console.log(parseInt(textNumber))
-let son = "buson";
-let sonu= son.substring(0, 3)
-let uch = 3
-let uzunligi= sonu.length
-let ahmoq = uzunligi=== uch ? "to'g'ri":"noto'ri";
-console.log(ahmoq)
-let number1 = "123"
-let number2 = "456"
-let r_number1 = parseInt(number1)
-let r_number2 = parseInt(number2)
-let add = r_number1+r_number2
-let tekshirish= add===parseInt("579") ? "To'g'ri":"Noto'g'ri"
-console.log(tekshirish)
-let nom = "Archimond"
-let nom_ch= nom.replace("Archimond", "Sargares")
-let nom_k = nom_ch.toLocaleUpperCase()
-let nom_ke = nom_k.slice(4)
-console.log(nom_ke)
-let aniqlash = nom_ke === nom_k.slice(4) ? "T":"N"
-console.log(aniqlash)         
+// let hour =prompt("Vaqtni kiritng")
+// if (hour<12){
+//     console.log("Good morning");
+// } else if(hour<18){
+//     console.log("Good afternoon");
+// }else{
+//     console.log("Good night")
+// }
+// let a = "text     ";
+// let b = "Again text";
+// console.log(a.concat(b));
+// console.log(a.length);
+// console.log(b.length);
+// console.log(a.slice(3));
+// console.log(a.slice(2));
+// console.log(a.slice(1));
+// console.log(a);
+// console.log(a.repeat(2));
+// console.log(a.repeat(2).length);
+// console.log(b.slice(5).concat(a));
+// console.log(a.replace("text", "changed text"));
+// console.log(a.padStart(20, "stupid"))
+// console.log(a.concat("is").padEnd(20, "stupid"));
+// console.log(a.substring(0, 2).concat(b.slice(8)));
+// console.log(a.includes("t"));
+// console.log(a.endsWith("ghdcfoubgvoykgu8'uyhgfhgdtfkyig,h   "));
+// console.log(a.concat(b).trim());
+// console.log(a.substring(0, 2).repeat(10));
+// let number =1234567;
+// console.log(number.toString())
+// let textNumber ="1234567";
+// console.log(Number(textNumber))
+// console.log(parseInt(textNumber))
+// let son = "buson";
+// let sonu= son.substring(0, 3)
+// let uch = 3
+// let uzunligi= sonu.length
+// let ahmoq = uzunligi=== uch ? "to'g'ri":"noto'ri";
+// console.log(ahmoq)
+// let number1 = "123"
+// let number2 = "456"
+// let r_number1 = parseInt(number1)
+// let r_number2 = parseInt(number2)
+// let add = r_number1+r_number2
+// let tekshirish= add===parseInt("579") ? "To'g'ri":"Noto'g'ri"
+// console.log(tekshirish)
+// let nom = "Archimond"
+// let nom_ch= nom.replace("Archimond", "Sargares")
+// let nom_k = nom_ch.toLocaleUpperCase()
+// let nom_ke = nom_k.slice(4)
+// console.log(nom_ke)
+// let aniqlash = nom_ke === nom_k.slice(4) ? "T":"N"
+// console.log(aniqlash)
+// // let nomi = qiymat;
+// let fAge= parseInt(prompt("Yoshni kiriting"));
+// let sAge= 9;
+// let qoshish= Math.abs(fAge)+sAge
+// console.log(qoshish)
+// let son2 = 24.2;
+// console.log(Math.round(son2)) // round yaqin songa yaxlitlaydi
+// console.log(Math.ceil(son2))  // keyingi songa otib yuborish
+// console.log(Math.floor(son2)) // asos ko'rsatish
+// console.log(Math.pow(son2, 5)) // tanlangani darajaga ko'paytirish
+// console.log(Math.sqrt(son2))   // ildiz
+// console.log(parseInt(Math.random()* 10/1)) // rendom
+// let uch_x_s = prompt("3 xonali son kiriting     ");
+// let kesamiz = uch_x_s.slice(2);
+// let kesamiz2 = uch_x_s.substring(0, 1);
+// let kesamiz3 = uch_x_s.substring(0, 2);
+// let kesamiz4 = kesamiz3.slice(1)
+// console.log(kesamiz.concat(kesamiz2, kesamiz4))
+// let uch_x_s = prompt("3 xonali son kiriting     ");
+// let kesamiz = uch_x_s.slice(2);
+// let kesamiz2 = uch_x_s.substring(0, 1);
+// let kesamiz3 = uch_x_s.substring(0, 2);
+// let kesamiz4 = kesamiz3.slice(1)
+// console.log(kesamiz2.concat(kesamiz, kesamiz4))
+         
